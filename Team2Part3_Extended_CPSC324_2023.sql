35e7c86c75ceb8b2731a18d321ec3f3e9daf143b
Derek <46918649+DBinnema@users.noreply.github.com>
2023-04-05 01:18:36 -0700

    Update Team2Part3_Extended_CPSC324_2023.sql

--- Team2Part3_Extended_CPSC324_2023.sql
+++ Team2Part3_Extended_CPSC324_2023.sql
@@ -20,7 +20,7 @@ WHERE plane.model_number = 'A10'
 
 SELECT *
 FROM flight 
-WHERE flight.flight_number = 912 OR flight_number = 256
+WHERE flight.flight_number = 912 OR flight.flight_number = 256
 
 --b
 
@@ -34,9 +34,38 @@ FROM employee_departure
 WHERE employee_departure.person_id = 15 OR customer_departure.person_id = 16
 ;
 
+SELECT *
+FROM departure JOIN plane ON departure.model_number = plane.model_number
+WHERE departure.model_number = 'A10'
+;
+
+SELECT *
+FROM departure JOIN flight ON departure.flight_number = flight.flight_number
+WHERE departure.flight_number = 912 OR departure.flight_number = 256
+;
+
+
 
 --c
 
---meaningful business question
+-- Management wants to figure out where to build a new airport,
+-- in order to determine what location would be most profitable,
+-- display how many flights each customer has taken in descending order and including zeros, alongside customer city in order to see what city populations fly the most.
+
+
+WITH count_destinations(person_id, flight_count) AS (
+SELECT c.person_id, COUNT(cd.departure_date) AS fly_count
+FROM customer AS c, customer_departure AS cd
+WHERE c.person_id = cd.person_id    
+GROUP BY c.person_id
+)
+
+SELECT p.city, SUM(d.flight_count) as flight_sum
+FROM person AS p, count_destinations AS d
+WHERE p.person_id = d.person_id
+GROUP BY p.city
+ORDER BY flight_sum DESC;
+
+
 
 
